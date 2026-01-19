e860f51f2ca66dad35dc327d40b79afd8e483ee0
KrazyPhish <krazyphish9495@gmail.com>
2026-01-19 11:09:18 +0800

    ci(root): :ferris_wheel: ci configs change

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,4 +1,10 @@
+const createLint = (target: string[]) => {
+  const isCi = process.env.CI
+  if (isCi) return []
+  return target
+}
+
 export default {
-  "*.{js,ts,jsx,tsx,vue,md}": ["prettier --write", "eslint"],
-  "*.{js,ts,mjs,cjs,json,tsx,css,less,scss,vue,html,md}": ["cspell lint"],
+  "*.{js,ts,jsx,tsx,vue,md}": createLint(["prettier --write", "eslint"]),
+  "*.{js,ts,mjs,cjs,json,tsx,css,less,scss,vue,html,md}": createLint(["cspell lint"]),
 }
